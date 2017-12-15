bb260c29a7438234d31ed0dc27e2f9b45fb3d7b7
Jennifer Bono <jennb24@gmail.com>
2017-12-15 14:12:36 -0600

    google fonts

--- server.js
+++ server.js
@@ -14,23 +14,29 @@ app.use(bodyParser.json());
 app.use(express.static('public'));
 
 app.get("/api/friends", function(request, response) {
-  response.json(friends);
+  	response.json(friends);
 });
 
 app.post("/api/friends", function(request, response) {
-  var friendsTotalScores = [];
-  for (var i = 0; i < friends.length; i++) {
-  	friendsTotalScores.push(friends[i].scores.reduce(getSum));
-  }
-  var myTotalScore = request.body.scores.reduce(getSum);
-  var currentClosestFriend;
-  var currentClosestFriendDifference;
-  
+	console.log(request.body);
+	var friendsTotalScores = [];
+	for (var i = 0; i < friends.length; i++) {
+		friendsTotalScores.push(friends[i].scores.reduce(getSum));
+	}
+	var myTotalScore = request.body.scores.reduce(getSum);
+	var closestFriendsDifference = friendsTotalScores.map(function(score){
+		return Math.abs(score - myTotalScore);
+	})
+	var closestFriendDifference = Math.min.apply(Math, closestFriendsDifference);
+	var closestFriendIndex = closestFriendsDifference.indexOf(closestFriendDifference);
+	var closestFriend = friends[closestFriendIndex];
+	response.json(closestFriend);
+	friends.push(request.body);
 });
 
 // Starts the server to begin listening
 app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
+  	console.log("App listening on PORT " + PORT);
 });
 
 function getSum(total, num) {
